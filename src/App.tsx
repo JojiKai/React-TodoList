b273b1c268ad41169d4c40197547d97f97a7b5a7
JojiKai_LapTop <whatever1668@gmail.com>
2025-07-30 14:35:54 +0800

    Add: enhanced todo features

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import { TodoItem } from "./Todos/TodoItem";
 import "bulma/css/bulma.min.css";
 import { TodoList } from "./Todos/TodoList";
+import { Editor } from "./Todos/Editor";
 
 export const App = () => {
   return (
     <>
-      <TodoList />
+      {/* <TodoList /> */}
+      <Editor/>
     </>
   );
 };
