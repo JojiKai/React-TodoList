04b97311c5afbf8df27a3fcd03df46be628fc243
JojiKai_LapTop <whatever1668@gmail.com>
2025-08-01 11:43:51 +0800

    86.Text input & 88.Radio

--- src/Todos/Editor.tsx
+++ src/Todos/Editor.tsx
@@ -1,29 +1,75 @@
-import { FC } from "react";
+import { ChangeEventHandler, FC, useRef, useState } from "react";
+import { Priority } from "./TodoItem";
+import { kebabCase } from "lodash";
 
 export const Editor: FC = () => {
+  const [title, setTitle] = useState<string>("");
+  const [priority, setPriority] = useState<Priority>(Priority.LOW);
+
+  const handleTitleChange: ChangeEventHandler<HTMLInputElement> = (e) =>
+    setTitle(e.target.value);
+
+  const handlePriorityChange: ChangeEventHandler<HTMLInputElement> = (e) =>
+    setPriority(parseInt(e.target.value));
+
   return (
     <div className="box">
       <div className="field">
         <div className="control">
-          <input type="text" className="input" placeholder="title" />
+          <input
+            type="text"
+            className="input"
+            placeholder="title"
+            value={title}
+            onChange={handleTitleChange}
+          />
         </div>
       </div>
       <div className="columns is-vcentered">
         <div className="column">
           <div className="field">
             <div className="control">
-              <label className="radio">
-                <input type="radio" />
+              {Object.entries(Priority)
+                .filter(([k, v]) => isNaN(k as any))
+                .map(([k, v]) => (
+                  <label className="radio" key={k}>
+                    <input
+                      type="radio"
+                      checked={priority === v}
+                      value={v}
+                      onChange={handlePriorityChange}
+                    />
+                    {" " + k}
+                  </label>
+                ))}
+
+              {/* <label className="radio">
+                <input
+                  type="radio"
+                  checked={priority === 0}
+                  value={Priority.HIGH}
+                  onChange={handlePriorityChange}
+                />
                 High
               </label>
               <label className="radio">
-                <input type="radio" />
+                <input
+                  type="radio"
+                  checked={priority === 1}
+                  value={Priority.MEDIUM}
+                  onChange={handlePriorityChange}
+                />
                 Medium
               </label>
               <label className="radio">
-                <input type="radio" />
+                <input
+                  type="radio"
+                  checked={priority === 2}
+                  value={Priority.LOW}
+                  onChange={handlePriorityChange}
+                />
                 Low
-              </label>
+              </label> */}
             </div>
           </div>
         </div>
@@ -62,7 +108,12 @@ export const Editor: FC = () => {
         <div className="field">
           <div className="control">
             <div className="button has-addons">
-              <button className="button is-link">Save</button>
+              <button
+                className="button is-link"
+                onClick={() => console.log(title)}
+              >
+                Save
+              </button>
               <button className="button is-link is-light">Cancel</button>
             </div>
           </div>
